e998b7da130c26810ba3fc355208be75cb954253
Domic <dominicbertocchini@gmail.com>
2020-12-28 10:20:01 -0800

    replaced previous level select with a scrolling menu

--- Scripts/Level Select.gd
+++ Scripts/Level Select.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends MarginContainer
 
 var levels = Array();
 
--- Scripts/Main Menu.gd
+++ Scripts/Main Menu.gd
@@ -5,7 +5,7 @@ onready var options_menu = get_node("HBoxContainer/VBoxContainer/Options/PopupMe
 onready var credits = get_node("HBoxContainer/VBoxContainer/Credits/Sprite");
 
 func _on_Continue_pressed():
-	Global.goto_scene("res://Scenes/Level Select.tscn");
+	Global.goto_scene("res://Scenes/Main Menu 2.tscn");
 
 func _on_New_Game_pressed():
 	save_menu.popup_centered();
