d65d9a935271b129c8f99c639225799ce9cf21ed
Domic <dominicbertocchini@gmail.com>
2021-01-04 10:00:45 -0800

    Resolution changed and option menu functionality framework

---
+++ Scripts/Options.gd
@@ -0,0 +1,35 @@
+extends MarginContainer
+
+# Drop down menus
+func _on_Resolution_item_selected(index):
+	pass # Replace with function body.
+
+func _on_DisplayMode_item_selected(index):
+	pass # Replace with function body.
+
+
+# Vsync
+func _on_TextureButton_toggled(button_pressed):
+	pass # Replace with function body.
+
+
+# Volume slider
+func _on_HSlider_value_changed(value):
+	pass # Replace with function body.
+
+
+# Keybinds
+func _on_Forwards_pressed():
+	pass # Replace with function body.
+
+func _on_Backwards_pressed():
+	pass # Replace with function body.
+
+func _on_Left_pressed():
+	pass # Replace with function body.
+
+func _on_Right_pressed():
+	pass # Replace with function body.
+
+func _on_Brake_pressed():
+	pass # Replace with function body.
