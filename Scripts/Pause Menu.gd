deeaeb2afe3cf981dfcfc8b3fef1daedc38d5ae5
Domic <dominicbertocchini@gmail.com>
2021-01-24 21:52:30 -0800

    pause menu button functionality

--- Scripts/Pause Menu.gd
+++ Scripts/Pause Menu.gd
@@ -5,10 +5,23 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("menu_toggle"):
-		print(self.visible);
-		print(self.rect_position);
 		if self.visible:
 			self.hide();
 		else:
 			self.popup();
 		get_tree().paused = !get_tree().paused;
+
+
+func _on_Resume_pressed():
+	self.hide();
+	get_tree().paused = false;
+
+
+func _on_Restart_pressed():
+	self.hide();
+	get_tree().paused = false;
+	Global.goto_scene(get_tree().current_scene.filename);
+
+
+func _on_Exit_pressed():
+	Global.goto_scene("res://Scenes/Main Menu 2.tscn");
